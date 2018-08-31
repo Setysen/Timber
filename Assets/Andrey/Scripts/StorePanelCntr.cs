83b5fe3aeba5353f508a10ea8dfb0d302f49df09
SuperAndy31 <42539930+SuperAndy31@users.noreply.github.com>
2018-08-31 12:42:36 +0300

    31.08.18 Актуальная версия

---
+++ Assets/Andrey/Scripts/StorePanelCntr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorePanelCntr : MonoBehaviour {
+
+    public bool stop = false;
+    public GameObject storePanel;
+    float speed = 10f;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        ////if ()
+        ////{
+        ////    stop = true;
+        ////}
+        //if (Input.GetButton("Vertical") && stop == false)
+        //{
+        //    storePanel.transform.position += storePanel.transform. * speed * Time.deltaTime;
+        //}
+        ////if (Input.GetButton("Vertical") && stop == false)
+        ////{
+        ////    storePanel.transform.position += storePanel.transform.forward * speed *  Time.deltaTime;
+        ////}
+	}
+}
