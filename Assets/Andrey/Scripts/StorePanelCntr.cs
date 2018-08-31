ea65bc828598eac6fa6717679f221b0fa7246c6d
SuperAndy31 <42539930+SuperAndy31@users.noreply.github.com>
2018-08-31 13:09:36 +0300

    31.08

---
+++ Assets/Andrey/Scripts/StorePanelCntr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorePanelCntr : MonoBehaviour {
+
+    public bool stop = false;
+    public GameObject storePanel;
+    float speed = 10f;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        //if ()
+        //{
+        //    stop = true;
+        //}
+        //if ( Input.GetButton (KeyCode.W) && stop == false)
+        //{
+        //    storePanel.transform.position = transform.position = new Vector3(storePanel.transform.position.x, storePanel.transform.position.y + 1, 0);
+        //}
+        //if ( Input.GetButton (KeyCode.S) && stop == false)
+        //{
+        //    storePanel.transform.position = transform.position = new Vector3(storePanel.transform.position.x, storePanel.transform.position.y - 1, 0);
+        //}
+	}
+}
