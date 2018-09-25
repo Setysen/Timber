a634c4956812d088cfc027d8c70919e90047cbb4
PinDanil <drobrorobro@gmail.com>
2018-09-25 09:03:18 +0300

    ебусь с кнопками

--- Assets/Andrey/Scripts/StorePanelCntr.cs
+++ Assets/Andrey/Scripts/StorePanelCntr.cs
@@ -2,45 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StorePanelCntr : MonoBehaviour {
+public class StorePanelCntr : MonoBehaviour
+{
 
     public bool stop = false;
     public GameObject storePanel;
     float speed = 10f;
 
-	void Start () 
+    void Start()
     {
-	
-	}
-	
-	void Update () 
+
+    }
+
+    void Update()
     {
-<<<<<<< HEAD
-        //if ()
-        //{
-        //    stop = true;
-        //}
-        //if ( Input.GetButton (KeyCode.W) && stop == false)
-        //{
-        //    storePanel.transform.position = transform.position = new Vector3(storePanel.transform.position.x, storePanel.transform.position.y + 1, 0);
-        //}
-        //if ( Input.GetButton (KeyCode.S) && stop == false)
-        //{
-        //    storePanel.transform.position = transform.position = new Vector3(storePanel.transform.position.x, storePanel.transform.position.y - 1, 0);
-        //}
-=======
-        ////if ()
-        ////{
-        ////    stop = true;
-        ////}
-        //if (Input.GetButton("Vertical") && stop == false)
-        //{
-        //    storePanel.transform.position += storePanel.transform. * speed * Time.deltaTime;
-        //}
-        ////if (Input.GetButton("Vertical") && stop == false)
-        ////{
-        ////    storePanel.transform.position += storePanel.transform.forward * speed *  Time.deltaTime;
-        ////}
->>>>>>> 83b5fe3aeba5353f508a10ea8dfb0d302f49df09
-	}
+        /*
+        if ()
+        {
+            stop = true;
+        }
+        if ( Input.GetButton (KeyCode.W) && stop == false)
+        {
+            storePanel.transform.position = transform.position = new Vector3(storePanel.transform.position.x, storePanel.transform.position.y + 1, 0);
+        }
+        if ( Input.GetButton (KeyCode.S) && stop == false)
+        {
+            storePanel.transform.position = transform.position = new Vector3(storePanel.transform.position.x, storePanel.transform.position.y - 1, 0);
+        }
+
+	*/
+    }
 }
